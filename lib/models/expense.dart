19c2da5b2dee1e06b89b2816bfb8bda5044db79e
hoangnv <hoangnv@ai-t.vn>
2024-01-26 15:05:32 +0700

    add chart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -44,7 +44,7 @@ class ExpenseBucket {
             .where((expense) => expense.category == category)
             .toList();
 
-  double get totalExpense {
+  double get totalExpenses {
     double sum = 0;
 
     for (final expense in expenses) {
